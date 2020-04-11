ba271a01c5c626bcd978727a1931c365f6f853f8
Jay <thk116@gmail.com>
2020-04-11 16:17:34 -0700

    OwnerEditComponent added and conditionally displays with empty fields for new item or filled fields for existing item edit

--- mobile/components/OwnerEditItem.jsx
+++ mobile/components/OwnerEditItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from "react-native";
-import { Header, Icon, Button, ListItem } from 'react-native-elements';
+import { StyleSheet, View, Text, TextInput, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Header, Icon, Button, ListItem, Input } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 import { HOST } from 'react-native-dotenv';
 import axios from 'axios';
@@ -9,12 +9,14 @@ export default class OwnerEditItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sideMenuView: false
+      sideMenuView: false,
+      existingItem: false
     }
   }
 
   componentDidMount() {
-      console.log('props: ', this.props)
+      console.log('props: ', this.props.itemData)
+      this.props.itemData ? this.setState({ existingItem: true }) : null
     //   axios.get(`http://192.168.1.65:3000/api/Businesses/${this.props.businessIds[0]}`)
     //     .then(res => {
     //         menu = res.data.menu
@@ -41,6 +43,45 @@ export default class OwnerEditItem extends React.Component {
                     //onPress={() => this.goToOwnerMap(this.props.token, this.props.userId, this.props.businessIds)}
                 />}
             />
+            <KeyboardAvoidingView  style={styles.container}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View>
+                        <Text style={styles.label}>Item Name</Text>
+                        <TextInput 
+                            style={styles.input}
+                            defaultValue={this.state.existingItem ? this.props.itemData.item : ''}
+                            placeholder={this.state.existingItem ? '' : 'Pommes Frites'}/>
+                        <Text style={styles.label}>Price</Text>
+                        <TextInput
+                            style={styles.input}
+                            defaultValue={this.state.existingItem ? this.props.itemData.price : ''}
+                            placeholder={this.state.existingItem ? '' : '9.99'}/>
+                        <Text style={styles.label}>Category</Text>
+                        <TextInput
+                            style={styles.input}
+                            defaultValue={this.state.existingItem ? this.props.itemData.category : ''}
+                            placeholder={this.state.existingItem ? '' : 'American, Chicken, Gourmet, etc.'}/>
+                        <Text style={styles.label}>Image</Text>
+                        <TextInput
+                            style={styles.input}
+                            defaultValue={this.state.existingItem ? this.props.itemData.image : ''}
+                            placeholder={this.state.existingItem ? '' : 'https://link-to-your-image.com...'}/>
+                        <Text style={styles.label}>Description</Text>
+                        <TextInput
+                            style={styles.textarea}
+                            defaultValue={this.state.existingItem ? this.props.itemData.desc : ''}
+                            placeholder={this.state.existingItem ? '' : 'Here is your dish"s chance to shine. Give all the details!'} multiline={true}/>
+                        <TouchableOpacity
+                            style={styles.submitButton}
+                            onPress={() => this.login(this.state.email, this.state.password)}>
+                            <Text style={styles.submitButtonText}> Submit </Text>
+                        </TouchableOpacity>
+                    </View>
+                </TouchableWithoutFeedback>
+                
+            </KeyboardAvoidingView>
+           
+            {/* {this.state.existingItem ? } */}
         </View>
       )
   }
@@ -50,5 +91,50 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffe599',
-    }
+    },
+    label: {
+        margin: 5,
+        color: 'black',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: 'white',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 15,
+        height: 40,
+        padding: 5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        fontSize: 18
+    },
+    textarea: {
+        backgroundColor: 'white',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 15,
+        height: 120,
+        padding: 5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        fontSize: 18
+    },
+    submitButton: {
+        backgroundColor: '#980000',
+        padding: 10,
+        marginTop: 25,
+        marginLeft: 100,
+        marginRight: 100,
+        marginBottom: 15,
+        height: 40,
+        borderRadius: 20,
+    },
+    submitButtonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize:17,
+    },
 })
