be550463dd39a9bbee34becccebaefdacb8064b9
Jay <thk116@gmail.com>
2020-04-11 18:28:54 -0700

    OwnerEditMenu component scrolls and avoids keyboard properly and popular item checkbox added

--- mobile/components/OwnerEditItem.jsx
+++ mobile/components/OwnerEditItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
-import { Header, Icon, Button, ListItem, Input } from 'react-native-elements';
+import { StyleSheet, View, Text, TextInput, KeyboardAvoidingView, ScrollView, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Header, Icon, Button, ListItem, Input, CheckBox } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 import { HOST } from 'react-native-dotenv';
 import axios from 'axios';
@@ -10,7 +10,8 @@ export default class OwnerEditItem extends React.Component {
     super(props);
     this.state = {
       sideMenuView: false,
-      existingItem: false
+      existingItem: false,
+      popularItem: false
     }
   }
 
@@ -27,62 +28,70 @@ export default class OwnerEditItem extends React.Component {
 
   render() {
     return (
-        <View style={styles.container}>
-            <Header
-                containerStyle={{
-                    backgroundColor: '#980000',
-                    justifyContent: 'space-around',
-                }}
-                leftComponent={<Icon
-                    name='menu'
-                    //onPress={() => this.toggleSideMenu(this.state.sideMenuView)}
-                />}
-                centerComponent={{ style: { color: '#fff', fontSize: 25, fontWeight: 'bold' }, text: "Edit Menu" }}
-                rightComponent={<Icon
-                    name='home'
-                    //onPress={() => this.goToOwnerMap(this.props.token, this.props.userId, this.props.businessIds)}
-                />}
-            />
-            <KeyboardAvoidingView  style={styles.container}>
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <View>
-                        <Text style={styles.label}>Item Name</Text>
-                        <TextInput 
-                            style={styles.input}
-                            defaultValue={this.state.existingItem ? this.props.itemData.item : ''}
-                            placeholder={this.state.existingItem ? '' : 'Pommes Frites'}/>
-                        <Text style={styles.label}>Price</Text>
-                        <TextInput
-                            style={styles.input}
-                            defaultValue={this.state.existingItem ? this.props.itemData.price : ''}
-                            placeholder={this.state.existingItem ? '' : '9.99'}/>
-                        <Text style={styles.label}>Category</Text>
-                        <TextInput
-                            style={styles.input}
-                            defaultValue={this.state.existingItem ? this.props.itemData.category : ''}
-                            placeholder={this.state.existingItem ? '' : 'American, Chicken, Gourmet, etc.'}/>
-                        <Text style={styles.label}>Image</Text>
-                        <TextInput
-                            style={styles.input}
-                            defaultValue={this.state.existingItem ? this.props.itemData.image : ''}
-                            placeholder={this.state.existingItem ? '' : 'https://link-to-your-image.com...'}/>
-                        <Text style={styles.label}>Description</Text>
-                        <TextInput
-                            style={styles.textarea}
-                            defaultValue={this.state.existingItem ? this.props.itemData.desc : ''}
-                            placeholder={this.state.existingItem ? '' : 'Here is your dish"s chance to shine. Give all the details!'} multiline={true}/>
-                        <TouchableOpacity
-                            style={styles.submitButton}
-                            onPress={() => this.login(this.state.email, this.state.password)}>
-                            <Text style={styles.submitButtonText}> Submit </Text>
-                        </TouchableOpacity>
-                    </View>
-                </TouchableWithoutFeedback>
-                
-            </KeyboardAvoidingView>
-           
-            {/* {this.state.existingItem ? } */}
-        </View>
+        <KeyboardAvoidingView  style={styles.container} behavior="padding">
+            <View>
+                <Header
+                    containerStyle={{
+                        backgroundColor: '#980000',
+                        justifyContent: 'space-around',
+                    }}
+                    leftComponent={<Icon
+                        name='menu'
+                        //onPress={() => this.toggleSideMenu(this.state.sideMenuView)}
+                    />}
+                    centerComponent={{ style: { color: '#fff', fontSize: 25, fontWeight: 'bold' }, text: "Edit Menu" }}
+                    rightComponent={<Icon
+                        name='home'
+                        //onPress={() => this.goToOwnerMap(this.props.token, this.props.userId, this.props.businessIds)}
+                    />}
+                />
+            </View>
+                    {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss}> */}
+                        <ScrollView scrollEnabled={true}>
+                            <Text style={styles.label}>Item Name</Text>
+                            <TextInput 
+                                style={styles.input}
+                                defaultValue={this.state.existingItem ? this.props.itemData.item : ''}
+                                placeholder={this.state.existingItem ? '' : 'Pommes Frites'}/>
+                            <Text style={styles.label}>Price</Text>
+                            <TextInput
+                                style={styles.input}
+                                defaultValue={this.state.existingItem ? this.props.itemData.price : ''}
+                                placeholder={this.state.existingItem ? '' : '9.99'}/>
+                            <Text style={styles.label}>Category</Text>
+                            <TextInput
+                                style={styles.input}
+                                defaultValue={this.state.existingItem ? this.props.itemData.category : ''}
+                                placeholder={this.state.existingItem ? '' : 'American, Chicken, Gourmet, etc.'}/>
+                            <Text style={styles.label}>Image</Text>
+                            <TextInput
+                                style={styles.input}
+                                defaultValue={this.state.existingItem ? this.props.itemData.image : ''}
+                                placeholder={this.state.existingItem ? '' : 'https://link-to-your-image.com...'}/>
+                            <Text style={styles.label}>Description</Text> 
+                            <TextInput
+                                style={styles.textarea}
+                                defaultValue={this.state.existingItem ? this.props.itemData.desc : ''}
+                                placeholder={this.state.existingItem ? '' : 'Here is your dish"s chance to shine. Give all the details!'} multiline={true}/>
+                            <CheckBox
+                                containerStyle={styles.checkbox}
+                                center
+                                title='Mark As Popular Item'
+                                checked={this.state.popularItem}
+                                onPress={() => this.setState({popularItem: !this.state.popularItem})}/>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.login(this.state.email, this.state.password)}>
+                                <Text style={styles.buttonText}> Cancel </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.login(this.state.email, this.state.password)}>
+                                <Text style={styles.buttonText}> Submit </Text>
+                            </TouchableOpacity>
+                        </ScrollView>
+                    {/* </TouchableWithoutFeedback> */}
+        </KeyboardAvoidingView>
       )
   }
 }
@@ -102,7 +111,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         marginLeft: 10,
         marginRight: 10,
-        marginBottom: 15,
+        marginBottom: 10,
         height: 40,
         padding: 5,
         borderColor: 'gray',
@@ -114,27 +123,32 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         marginLeft: 10,
         marginRight: 10,
-        marginBottom: 15,
+        marginBottom: 10,
         height: 120,
         padding: 5,
         borderColor: 'gray',
         borderWidth: 1,
+        borderRadius: 5,
         fontSize: 18
     },
-    submitButton: {
+    button: {
         backgroundColor: '#980000',
         padding: 10,
-        marginTop: 25,
+        marginTop: 20,
         marginLeft: 100,
         marginRight: 100,
-        marginBottom: 15,
         height: 40,
         borderRadius: 20,
     },
-    submitButtonText: {
+    buttonText: {
         color: 'white',
         textAlign: 'center',
         fontWeight: 'bold',
         fontSize:17,
     },
+    checkbox: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+    }
 })
