965a65fe61bb2230e1be53bf568fe9f5def46e9b
Jay <thk116@gmail.com>
2020-04-16 15:47:10 -0700

    testing branch

--- mobile/components/Register.jsx
+++ mobile/components/Register.jsx
@@ -72,7 +72,7 @@ class Register extends Component {
                         underlineColorAndroid='transparent'
                         autoCapitalize='none'
                         onChangeText={this.handleName}>
-                        Name
+                        NAME RIGHT HERE!!!!!!!!!
                         </AnimatedInput>
                     <AnimatedInput
                         inputStyle={styles.input}
